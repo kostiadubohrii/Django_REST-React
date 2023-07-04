1835708a347b76a10dc089e9cdb9072d440b2939
= <kostadubogriy@gmail.com>
2023-07-04 14:27:11 +0100

    add uesr api

--- rest-api_example/drinks/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from users import views
-
-urlpatterns = [
-    path('users/', views.user_list),
-    path('users/<string:name><int:id>', views.user_detail),
-]
